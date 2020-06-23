56784d0e70364996ea4325c30a1c5e7aae2d0e92
adpines <pinesa@pennmedicine.upenn.edu>
2020-06-23 15:13:28 -0400

    minor minor changes (rmd to include oldfc file for confirmation, conc_sfs.sh used to screen pgs deviance)

--- scripts/viz/concatenate_scalefigs.sh
+++ scripts/viz/concatenate_scalefigs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 A="/cbica/projects/pinesParcels/multiscale/scripts/viz/concatenate_scalefigs.sh"
-for i in /cbica/projects/pinesParcels/results/viz/Gro_Con_Kequal_*; do
+A=""
+for i in /cbica/projects/pinesParcels/results/viz/Subj8*_infl_tm1.png; do
 	A+=" ${i}"
 done
 echo "$A "
-exec montage $A  -geometry +3+10 /cbica/projects/pinesParcels/results/viz/Group_partitions_over_scales.png
+exec montage $A  -geometry +3+10 /cbica/projects/pinesParcels/results/viz/deviant8s.png
