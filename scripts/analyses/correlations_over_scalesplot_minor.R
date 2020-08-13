4715bd10c196def343969f86d35093ee31ffd203
adpines <pinesa@pennmedicine.upenn.edu>
2020-08-13 10:05:00 -0400

    visualization updates

--- scripts/analyses/correlations_over_scalesplot_minor.R
+++ scripts/analyses/correlations_over_scalesplot_minor.R
@@ -1,12 +1,12 @@
-correlations_over_scales <- function(correlations,title){
+correlations_over_scales_minor <- function(correlations,title){
   df<-data.frame(correlations)
   ggplot(df,aes(X1,X2)) + geom_step(size=3)+ylab(expression(rho))+
-    theme_minimal(base_size = 35)+
+    theme_minimal(base_size = 31)+
     theme(axis.text=element_text(), axis.title = element_text(),panel.grid.minor = element_blank(), plot.title = element_text())+
     xlab("# of Communities") + scale_x_continuous(breaks=seq(2, 30, 4)) +
     ggtitle(title)+
     geom_hline(aes(yintercept=.098), linetype="dashed", col='#ECB602',size=2.5) +
-    geom_hline(aes(yintercept=-.098), linetype="dashed", col='#ECB602',size=2.5)+ylim(c(-.4,.4))
+    geom_hline(aes(yintercept=-.098), linetype="dashed", col='#ECB602',size=2.5)
 }
 
 
