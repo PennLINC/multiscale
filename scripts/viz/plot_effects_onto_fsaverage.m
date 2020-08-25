53698c558ba2d3f1bcefed98356e123c0442981b
adpines <pinesa@pennmedicine.upenn.edu>
2020-08-24 22:21:41 -0400

    age effect size quant, reverse HERE if you want to go back to agesegreg cor (or use it to check ads./removs.

--- scripts/viz/plot_effects_onto_fsaverage.m
+++ scripts/viz/plot_effects_onto_fsaverage.m
@@ -2,15 +2,15 @@
 
 addpath('/cbica/projects/pinesParcels/multiscale/scripts/derive_parcels/Toolbox');
 vecsfp='/cbica/projects/pinesParcels/results/EffectVecs';
-vecs=dir([vecsfp '/*mSeg.csv']);
+%vecs=dir([vecsfp '/*mSeg.csv']);
 %uncomment to switch to EF
-%vecs=dir([vecsfp '/*_EF.csv']);
+vecs=dir([vecsfp '/*_EF.csv']);
 % first two are . and ..
 sizevecs=size(vecs);
 for i=1:sizevecs(1)
-	% Set to AGE for this version. Change lines 3 and 5 if you switch this one. and the _Age.csv fn a few lines down
-	effectname='mSeg'
-	%effectname='EF'
+	% Set to EF for this version. Change lines 3 and 5 if you switch this one. and the _Age.csv fn a few lines down
+	%ffectname='mSeg'
+	effectname='EF'
 	% i + 1 because 1:29 files but 2:30 scales
 	scale=i+1;
 	% have to manually reconstruct the name bc bash stores as 10 first
