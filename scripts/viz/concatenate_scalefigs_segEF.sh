80fdfacaca137494bb17f87624f673843885d36e
adpines <pinesa@pennmedicine.upenn.edu>
2020-08-25 21:16:58 -0400

    EF effect mapping, R^2 updated to be adjusted R^2 in age effect modeling, viz updates.

--- scripts/viz/concatenate_scalefigs_segEF.sh
+++ scripts/viz/concatenate_scalefigs_segEF.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# 8/25 - CHANGED to just run on group or individualized-partition fc values
+
 A="/cbica/projects/pinesParcels/multiscale/scripts/viz/concatenate_scalefigs.sh"
-for i in /cbica/projects/pinesParcels/results/viz/Group_Effect_at*_EF*; do
+for i in /cbica/projects/pinesParcels/results/viz/Group_Effect_at*_EF_g*; do
 	A+=" ${i}"
 done
 echo "$A "
-exec montage $A  -geometry +3+10 /cbica/projects/pinesParcels/results/viz/Group_part_EFSegregEffect_over_scales.png
+exec montage $A  -geometry +3+10 /cbica/projects/pinesParcels/results/viz/Group_part_EFSegregEffect_over_scales_g.png
