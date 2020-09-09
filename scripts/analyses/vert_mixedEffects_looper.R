a992c9a5e004b2901fe73f3d6cb25ec797f38072
adpines <pinesa@pennmedicine.upenn.edu>
2020-09-09 19:22:19 -0400

    further betweenifying

--- scripts/analyses/vert_mixedEffects_looper.R
+++ scripts/analyses/vert_mixedEffects_looper.R
@@ -1,14 +1,14 @@
-.libPaths('/cbica/projects/pinesParcels/R')
+# look at previous version on github (prior to 9/9/20) for cubic filepaths
 library(reshape2)
 library(lme4)
-demo<-read.csv('/cbica/projects/pinesParcels/data/pnc_demo.csv')
+demo<-read.csv('~/pnc_demo.csv')
+subjects<-read.csv('~/participants.txt',header = F)
 ageSex<-data.frame(demo$ageAtScan1,as.factor(demo$sex),demo$scanid,demo$bblid)
-subjects<-read.csv('/cbica/projects/pinesParcels/data/participants.txt',header = F)
 colnames(ageSex)[4]<-'bblid'
 colnames(ageSex)[1]<-'Age'
-Rest_Motion_Data <- read.csv("/cbica/projects/pinesParcels/data/n1601_RestQAData_20170714.csv")
-NBack_Motion_Data <- read.csv("/cbica/projects/pinesParcels/data/n1601_NBACKQAData_20181001.csv")
-Idemo_Motion_Data <- read.csv("/cbica/projects/pinesParcels/data/n1601_idemo_FinalQA_092817.csv")
+Rest_Motion_Data <- read.csv("~/n1601_RestQAData_20170714.csv")
+NBack_Motion_Data <- read.csv("~/n1601_NBACKQAData_20181001.csv")
+Idemo_Motion_Data <- read.csv("~/n1601_idemo_FinalQA_092817.csv")
 motmerge<-merge(Rest_Motion_Data,NBack_Motion_Data,by='bblid')
 motmerge<-merge(motmerge,Idemo_Motion_Data,by='bblid')
 motmerge$Motion <- (motmerge$restRelMeanRMSMotion + motmerge$nbackRelMeanRMSMotion + motmerge$idemoRelMeanRMSMotion)/3;
@@ -19,13 +19,12 @@ colnames(ageSex)<-c("Age","Sex","scanid","bblid")
 df<-merge(subjects,ageSex,by="scanid")
 df<-merge(df,motiondf,by='bblid')
 
-outputcolnames=c('avgSlope','avgInt','ageSlope','ageInt')
-outarray=matrix(1,nrow=1,ncol=4)
+outputcolnames=c('avgSlope','avgInt','ageSlope','ageInt','MotionFixedEF')
+outarray=matrix(1,nrow=1,ncol=5)
 colnames(outarray)=outputcolnames
-
 for (v in 1:17734){
   print(v)
-  vFP=paste('/cbica/projects/pinesParcels/results/mixedEffectModels/v',v,'_bwVals_overScales.csv',sep='')
+  vFP=paste('~/mixedEffectModels/v',v,'_bwVals_overScales.csv',sep='')
   verts=read.csv(vFP)
   colnames(verts)[30]<-'bblid'
   df_verts<-merge(df,verts,by="bblid")
@@ -40,11 +39,13 @@ for (v in 1:17734){
   re=re$bblid
   avgSlope=fe['log(Scale)']
   avgInt=fe['(Intercept)']
+  avgMot=fe['Motion']
   ageSlope=cor.test(re$`log(Scale)`,df$Age,method='spearman')$estimate
   ageInt=cor.test(re$`(Intercept)`,df$Age,method='spearman')$estimate
   outarray[1]=avgSlope
   outarray[2]=avgInt
   outarray[3]=ageSlope
   outarray[4]=ageInt
-  write.csv(outarray,file=paste('/cbica/projects/pinesParcels/results/mixedEffectModels/Modeled_v',v,'_bwVals_overScales.csv',sep=''))
+  outarray[5]=avgMot
+  write.table(outarray,file=paste('~/mixedEffectModels/Modeled_fS_fI_raS_raI_fM_v',v,'_bwVals_overScales.csv',sep=''),sep=',',row.names=F,quote=F)
 }
