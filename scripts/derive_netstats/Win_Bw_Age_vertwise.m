62113c7bae3ea00a5037fb3ba561d2bc90e95387
adpines <pinesa@pennmedicine.upenn.edu>
2020-09-15 18:42:26 -0400

    various updates, largely related to unitary fixed effect depiction, FDR correction adds, PG2 depiction, viz updates

--- scripts/derive_netstats/Win_Bw_Age_vertwise.m
+++ scripts/derive_netstats/Win_Bw_Age_vertwise.m
@@ -35,38 +35,39 @@ bwstruct=load('/cbica/projects/pinesParcels/results/aggregated_data/ind_vertices
 % initialize empty matrix for age effects at each scale
 WinAgeEff=zeros(17734,29);
 BwAgeEff=zeros(17734,29);
+BwAgeSig=zeros(17734,29);
 
 %7/13/20 - used subj at index 542 to confirm alignment of ids - same 1st 10 vert pcs at scale 1 as from individual folder (individual pc_metrics.mat)
 parNetmat=table2array(nnet(:,2:4));
 
-%%%%%%%%%%%%%%%%%%%% %9/1/20 - pulled out example vertices
+%%%%%%%%%%%%%%%%%%%% %9/1/20 - pulled out example vertices%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 9/8 - adapted to sample all vertices
 % need a dataframe of subjects over scales for one vertex at a time
 
 % use vertex 40 for left motor, vertex 50 for left pfc,  vertexi 30 for vertex superior to left occipital pole
 
 % Read in subjects list
-subjs=load('/cbica/projects/pinesParcels/data/bblids.txt');
+%subjs=load('/cbica/projects/pinesParcels/data/bblids.txt');
 
-for v=1:17734
-	
-	% just to track loop progress
-	v=v
-	
-	% extract between values across scales for this vertex
-	Vertices_bwvals=squeeze(bwstruct.dfbw(v,:,:));
-
-	% slap on subject list to first column for matching in r
-	Vertices_bwvals(30,:)=subjs;
-
-	% transpose so subjects are rows (column will be subject ID)
-	Vertices_bwvals=Vertices_bwvals.';
-
-	fnV=strjoin(['/cbica/projects/pinesParcels/results/mixedEffectModels/v' string(v) '_bwVals_overScales.csv'],'');
-	writetable(table(Vertices_bwvals),fnV);
-end
-
-%%%%%%%%%%%%%%% End of vertices across subjects and scales sidecar
+%for v=1:17734
+%	
+%	% just to track loop progress
+%	v=v
+%	
+%	% extract between values across scales for this vertex
+%	Vertices_bwvals=squeeze(bwstruct.dfbw(v,:,:));
+%
+%	% slap on subject list to first column for matching in r
+%	Vertices_bwvals(30,:)=subjs;
+%
+%	% transpose so subjects are rows (column will be subject ID)
+%	Vertices_bwvals=Vertices_bwvals.';
+%
+%	fnV=strjoin(['/cbica/projects/pinesParcels/results/mixedEffectModels/v' string(v) '_bwVals_overScales.csv'],'');
+%	writetable(table(Vertices_bwvals),fnV);
+%end
+%
+%%%%%%%%%%%%%%% End of vertices across subjects and scales sidecar%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
@@ -79,11 +80,15 @@ for K=Krange
 		[ageCor,p]=partialcorr(parNetmat,'Type','Spearman');
 		% we want the 4th element (comes in table format depicted beneath)
 		WinAgeEff(v,K-1)=ageCor(4);
-		%%% and for negative values
+		
+		% adding in FDR correction 9/15/20
 		parNetmat(:,4)=BwK(v,:)';
                 [ageCor,p]=partialcorr(parNetmat,'Type','Spearman');
+		% p value vector
+		BwAgeSig(v,K-1)=p(4);
 		BwAgeEff(v,K-1)=ageCor(4);
 		end
+
 	%   Age|Motion|Sex|PC|
 	%Age   
 	%Motion
@@ -95,73 +100,75 @@ end
 fn=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspear.mat'];
 save(fn,'WinAgeEff');
 
-% repeat for Bw
+% repeat for Bw, add in fdr corrected pvals
+pfn=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearSig.mat'];
+save(pfn,'BwAgeSig');
 
 fn=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspear.mat'];
 save(fn,'BwAgeEff');
 
 %%%%%%%%%%%%%%%%%%%%%% A look at vertex-wise age-relation slope
 % total 2-30 dif
-WinAgeTotDif=WinAgeEff(:,29)-WinAgeEff(:,1);
-BwAgeTotDif=BwAgeEff(:,29)-BwAgeEff(:,1);
+%WinAgeTotDif=WinAgeEff(:,29)-WinAgeEff(:,1);
+%BwAgeTotDif=BwAgeEff(:,29)-BwAgeEff(:,1);
 % difference between 2 and 7
-WinAgeCoarseDif=WinAgeEff(:,8)-WinAgeEff(:,1);
-BwAgeCoarseDif=BwAgeEff(:,8)-BwAgeEff(:,1);
+%WinAgeCoarseDif=WinAgeEff(:,8)-WinAgeEff(:,1);
+%BwAgeCoarseDif=BwAgeEff(:,8)-BwAgeEff(:,1);
 % dif b/w scales 8 and 17
-WinAgeMidDif=WinAgeEff(:,18)-WinAgeEff(:,9);
-BwAgeMidDif=BwAgeEff(:,18)-BwAgeEff(:,9);
+%WinAgeMidDif=WinAgeEff(:,18)-WinAgeEff(:,9);
+%BwAgeMidDif=BwAgeEff(:,18)-BwAgeEff(:,9);
 % dif b/w scales 18 and 30 
-WinAgeFineDif=WinAgeEff(:,29)-WinAgeEff(:,19);
-BwAgeFineDif=BwAgeEff(:,29)-BwAgeEff(:,19);
+%WinAgeFineDif=WinAgeEff(:,29)-WinAgeEff(:,19);
+%BwAgeFineDif=BwAgeEff(:,29)-BwAgeEff(:,19);
 
 % actual correlations fit to each vertices' age relation over scales
-WinAgeEffScalesCor=zeros(17734,1);
-BwAgeEffScalesCor=zeros(17734,1);
-for v=1:17734
-	WinOverScales=WinAgeEff(v,:);
-	BwOverScales=BwAgeEff(v,:);
-	WinAgeEffScalesCor(v)=corr(WinOverScales',Krange');
-	BwAgeEffScalesCor(v)=corr(BwOverScales',Krange');
-end
+%WinAgeEffScalesCor=zeros(17734,1);
+%BwAgeEffScalesCor=zeros(17734,1);
+%for v=1:17734
+%	WinOverScales=WinAgeEff(v,:);
+%	BwOverScales=BwAgeEff(v,:);
+%	WinAgeEffScalesCor(v)=corr(WinOverScales',Krange');
+%	BwAgeEffScalesCor(v)=corr(BwOverScales',Krange');
+%end
 
 % change above scale 7
-fineKrange=8:30;
-fWinAgeEffScalesCor=zeros(17734,1);
-fBwAgeEffScalesCor=zeros(17734,1);
-for v=1:17734
-        WinOverScales=WinAgeEff(v,7:29);
-        BwOverScales=BwAgeEff(v,7:29);
-        fWinAgeEffScalesCor(v)=corr(WinOverScales',fineKrange');
-        fBwAgeEffScalesCor(v)=corr(BwOverScales',fineKrange');
-end
+%fineKrange=8:30;
+%fWinAgeEffScalesCor=zeros(17734,1);
+%fBwAgeEffScalesCor=zeros(17734,1);
+%for v=1:17734
+ %       WinOverScales=WinAgeEff(v,7:29);
+ %       BwOverScales=BwAgeEff(v,7:29);
+  %      fWinAgeEffScalesCor(v)=corr(WinOverScales',fineKrange');
+   %     fBwAgeEffScalesCor(v)=corr(BwOverScales',fineKrange');
+%end
 
 % write out ageCorChanges
-fnT=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearTotChange.mat'];
-fnC=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearCoarseChange.mat'];
-fnM=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearMidChange.mat'];
-fnF=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearFineChange.mat'];
-save(fnT,'WinAgeTotDif');
-save(fnC,'WinAgeCoarseDif');
-save(fnM,'WinAgeMidDif');
-save(fnF,'WinAgeFineDif');
+%fnT=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearTotChange.mat'];
+%fnC=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearCoarseChange.mat'];
+%fnM=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearMidChange.mat'];
+%fnF=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinspearFineChange.mat'];
+%save(fnT,'WinAgeTotDif');
+%save(fnC,'WinAgeCoarseDif');
+%save(fnM,'WinAgeMidDif');
+%save(fnF,'WinAgeFineDif');
 
 % and for between
 % write out ageCorChanges
-fnT=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearTotChange.mat'];
-fnC=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearCoarseChange.mat'];
-fnM=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearMidChange.mat'];
-fnF=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearFineChange.mat'];
-save(fnT,'BwAgeTotDif');
-save(fnC,'BwAgeCoarseDif');
-save(fnM,'BwAgeMidDif');
-save(fnF,'BwAgeFineDif');
+%fnT=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearTotChange.mat'];
+%fnC=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearCoarseChange.mat'];
+%fnM=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearMidChange.mat'];
+%fnF=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwspearFineChange.mat'];
+%save(fnT,'BwAgeTotDif');
+%save(fnC,'BwAgeCoarseDif');
+%save(fnM,'BwAgeMidDif');
+%save(fnF,'BwAgeFineDif');
 
 % write out correlation (slope) of vertexwise age correlations over scales
-fnW=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinCor_x_ScaleCor.mat'];
-fnBW=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwCor_x_ScaleCor.mat'];
-ffnW=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinCor_x_ScaleCorFine.mat'];
-ffnBW=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwCor_x_ScaleCorFine.mat'];
-save(fnW,'WinAgeEffScalesCor');
-save(fnBW,'BwAgeEffScalesCor');
-save(ffnW,'fWinAgeEffScalesCor');
-save(ffnBW,'fBwAgeEffScalesCor');
+%fnW=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinCor_x_ScaleCor.mat'];
+%fnBW=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwCor_x_ScaleCor.mat'];
+%ffnW=['/cbica/projects/pinesParcels/results/EffectVecs/ageWinCor_x_ScaleCorFine.mat'];
+%ffnBW=['/cbica/projects/pinesParcels/results/EffectVecs/ageBwCor_x_ScaleCorFine.mat'];
+%save(fnW,'WinAgeEffScalesCor');
+%save(fnBW,'BwAgeEffScalesCor');
+%save(ffnW,'fWinAgeEffScalesCor');
+%save(ffnBW,'fBwAgeEffScalesCor');
