d72e38702625f75e2a708eb6bd2bba6984622f37
adpines <pinesa@pennmedicine.upenn.edu>
2020-10-22 19:51:27 -0400

    SpatChange fix

--- scripts/derive_spatialprops/aggregate_changeVectors.m
+++ scripts/derive_spatialprops/aggregate_changeVectors.m
@@ -1,13 +1,21 @@
 %%% aggregate vertex-wise change over scales, print out in R friendly format
 % addpaths
 addpath(genpath('/cbica/projects/pinesParcels/multiscale/scripts/derive_parcels/Toolbox'));
-
-% reference surfaces
+% load in to exclude border vertices
+borderVertsfileL=load('/gpfs/fs001/cbica/projects/pinesParcels/results/aggregated_data/Border_excludeVec_2_L.mat');
+borderVertsfileR=load('/gpfs/fs001/cbica/projects/pinesParcels/results/aggregated_data/Border_excludeVec_2_R.mat');
+borderVertsL=borderVertsfileL.VertexExclude;
+borderVertsR=borderVertsfileR.VertexExclude;
+% convert to vertex indices to tag onto mask
+bordIndL=find(borderVertsL);
+bordIndR=find(borderVertsR);
 surfML = '/cbica/projects/pinesParcels/data/H_SNR_masks/lh.Mask_SNR.label';
 mwIndVec_l = read_medial_wall_label(surfML);
+mwIndVec_bord_l = vertcat(mwIndVec_l,bordIndL');
 Index_l = setdiff([1:10242], mwIndVec_l);
 surfMR = '/cbica/projects/pinesParcels/data/H_SNR_masks/rh.Mask_SNR.label';
 mwIndVec_r = read_medial_wall_label(surfMR);
+mwIndVec_bord_r = vertcat(mwIndVec_r,bordIndR');
 Index_r = setdiff([1:10242], mwIndVec_r);
 
 % initialize array (1 for each hemi, can merge later in R)
@@ -38,7 +46,6 @@ for h=1:2;
 		end
 	end
 end
-
 % save out files in r-friendlty format
 writetable(table(LHchangeVecs),'/cbica/projects/pinesParcels/results/aggregated_data/vertexWiseChange_LH.csv');
 writetable(table(RHchangeVecs),'/cbica/projects/pinesParcels/results/aggregated_data/vertexWiseChange_RH.csv');
