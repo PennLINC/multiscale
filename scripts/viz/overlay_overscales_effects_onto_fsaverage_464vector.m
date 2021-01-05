ba070ab403d16c4389103713427b917616e7e783
adpines <pinesa@pennmedicine.upenn.edu>
2021-01-05 15:58:20 -0500

    new feature inputs for overscalesoverlay

--- scripts/viz/overlay_overscales_effects_onto_fsaverage_464vector.m
+++ scripts/viz/overlay_overscales_effects_onto_fsaverage_464vector.m
@@ -2,9 +2,8 @@
 
 addpath(genpath('/cbica/projects/pinesParcels/multiscale/scripts/derive_parcels/Toolbox'));
 vecsfp='/cbica/projects/pinesParcels/results/EffectVecs';
-% _i is for individualized, added to look at individ. vs gro. partition effect differences
-% _g for group partitions
-vecs=dir([vecsfp '/Deriv10yoAt*']);
+effectname='Avg_k'
+vecs=dir([vecsfp '/Avg_k*']);
 % first two are . and ..
 for K=2:30
 	K_start=((K-1)*(K))/2;
@@ -17,6 +16,5 @@ for K=2:30
 	effCellStruct{K-1}=file;
 end
 % name
-effectname='Deriv10yoAt'
 PBP_effect_msOverlay_2View_R_lPFC(effCellStruct,effectname);
 %PBP_effect_msOverlay(effCellStruct,effectname);
--- scripts/viz/overlay_overscales_effects_onto_fsaverage_avg.m
+++ scripts/viz/overlay_overscales_effects_onto_fsaverage_avg.m
@@ -1,21 +1,15 @@
-
-
 addpath(genpath('/cbica/projects/pinesParcels/multiscale/scripts/derive_parcels/Toolbox'));
 vecsfp='/cbica/projects/pinesParcels/results/EffectVecs';
-%vecs=dir([vecsfp '/*mSeg.csv']);
-%uncomment to switch to meanSegreg
-% _i is for individualized, added to look at individ. vs gro. partition effect differences
-% _g for group partitions
-vecs=dir([vecsfp '/BW_InterceptAt*']);
+effectname='BW_InterceptAt21_k'
+%effectname='Avg_k'
+vecs=dir([vecsfp '/' effectname '*']);
 % first two are . and ..
 sizevecs=size(vecs);
-for i=1:29
+for i=1:sizevecs(1)
 	% i + 1 because 1:29 files but 2:30 scales
 	scale=i+1;
 	% have to manually reconstruct the name bc bash stores as 10 first
-	fn=['BW_InterceptAt' num2str(scale)];
+	fn=[effectname num2str(scale)];
 	effCellStruct{i}=load([vecsfp '/' fn]);
 end
-% name
-effectname='BW_InterceptAt'
 PBP_effect_msOverlay_2View(effCellStruct,effectname);
