10bf37f96c1c0ff8a94fe45c521be87542ad4cec
adpines <pinesa@pennmedicine.upenn.edu>
2021-01-30 18:12:40 -0500

    figure 3 estimated b/w intercept and age effect slopes

--- scripts/viz/overlay_overscales_effects_onto_fsaverageDeriv.m
+++ scripts/viz/overlay_overscales_effects_onto_fsaverageDeriv.m
@@ -1,21 +1,20 @@
 
-
+% get the tools
 addpath(genpath('/cbica/projects/pinesParcels/multiscale/scripts/derive_parcels/Toolbox'));
+% load in hand-off network-level age effect derivative vectors from R
 vecsfp='/cbica/projects/pinesParcels/results/EffectVecs';
-%vecs=dir([vecsfp '/*mSeg.csv']);
-%uncomment to switch to meanSegreg
-% _i is for individualized, added to look at individ. vs gro. partition effect differences
-% _g for group partitions
+% change between 10, 16, and 21 in the next two rows to run on various agepoints
 vecs=dir([vecsfp '/Deriv16yoAt*']);
-% name
 effectname='Deriv16yoAt'
-% first two are . and ..
+% get size of vectors
 sizevecs=size(vecs);
 for i=1:sizevecs(1)
 	% i + 1 because 1:29 files but 2:30 scales
 	scale=i+1;
-	% have to manually reconstruct the name bc bash stores as 10 first
+	% have to manually reconstruct the name bc bash stores files in its own order
 	fn=[effectname num2str(scale)];
+	% make a cell structure for matlab visualization function
 	effCellStruct{i}=load([vecsfp '/' fn]);
 end
+% run matlab visualization function for network-level age effect derivatives averaged over scales
 PBP_effect_msOverlay_2View_R_lPFC(effCellStruct,effectname);
--- scripts/viz/overlay_overscales_effects_onto_fsaverage_intercept.m
+++ scripts/viz/overlay_overscales_effects_onto_fsaverage_intercept.m
@@ -1,21 +1,18 @@
 
-
+% add toolbox path
 addpath(genpath('/cbica/projects/pinesParcels/multiscale/scripts/derive_parcels/Toolbox'));
 vecsfp='/cbica/projects/pinesParcels/results/EffectVecs';
-%vecs=dir([vecsfp '/*mSeg.csv']);
-%uncomment to switch to meanSegreg
-% _i is for individualized, added to look at individ. vs gro. partition effect differences
-% _g for group partitions
-vecs=dir([vecsfp '/BW_InterceptAt*']);
+% add R-printout estimated b/w intercepts at each age - replace At10 with At21 if estimate at 21 desired
+vecs=dir([vecsfp '/BW_InterceptAt10_k*']);
 % first two are . and ..
 sizevecs=size(vecs);
 for i=1:29
 	% i + 1 because 1:29 files but 2:30 scales
 	scale=i+1;
-	% have to manually reconstruct the name bc bash stores as 10 first
-	fn=['BW_InterceptAt' num2str(scale)];
+	% have to manually reconstruct the name bc bash stores as 10 first - same not as above applies, replace 10 if desired
+	fn=['BW_InterceptAt10_k' num2str(scale)];
 	effCellStruct{i}=load([vecsfp '/' fn]);
 end
-% name
-effectname='BW_InterceptAt'
+% name of printed brainmap
+effectname='Name'
 PBP_effect_msOverlay_2View(effCellStruct,effectname);
